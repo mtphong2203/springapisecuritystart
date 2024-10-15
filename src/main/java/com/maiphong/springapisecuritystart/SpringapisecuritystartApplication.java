3bbc5943c43df66e3bf174568b99a67c5207b848
Tien Phong <tienphong2203@gmail.com>
2024-10-15 18:39:09 +0700

    Create base project spring boot without config security

---
+++ src/main/java/com/maiphong/springapisecuritystart/SpringapisecuritystartApplication.java
@@ -0,0 +1,13 @@
+package com.maiphong.springapisecuritystart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringapisecuritystartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringapisecuritystartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maiphong/springapisecuritystart/SpringapisecuritystartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maiphong.springapisecuritystart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringapisecuritystartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
